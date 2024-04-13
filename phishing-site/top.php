09bc700b32f1cc1803328a25c7c5d3bcb62ec334
Eric Edlund <ericedlund2017@gmail.com>
2024-04-13 15:28:26 -0400

    Add materialize

--- phishing-site/top.php
+++ phishing-site/top.php
@@ -13,7 +13,13 @@ $pathParts=pathinfo($phpSelf);
         <meta name="description" content="Phishing Project for the 2024 UVM Hackathon">
         <link rel="SHORTCUT ICON" type="image/x-icon" href="images/logo.ico">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
+
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            
         
         <link rel="stylesheet" 
         href="css/custom.css?version=<?php print time(); ?>" 
